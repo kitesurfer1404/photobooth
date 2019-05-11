92d1026490308e50ad0ca35e1f66a9d07d8b08c8
kitesurfer1404 <github@aldick.org>
2019-05-11 19:38:37 +0200

    Updates shutdown.sh
    Better version with 5s press-and-hold delay.

--- raspi/photobooth/shutdown.py
+++ raspi/photobooth/shutdown.py
@@ -1,3 +1,4 @@
+#!/bin/python
 # Connect a momentary switch between GND and this pin.
 # As soon as this pin is switched to ground, the RasPi will shut down.
 # For numbering see: 
@@ -7,15 +8,22 @@ shutdown_pin = 26
 ################################################################################
 import RPi.GPIO as GPIO
 import subprocess
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-try:
-  while True:
+def shutdown(channel):
+    time.sleep(5)
     if not GPIO.input(shutdown_pin):
-      proc = subprocess.Popen(["sudo", "halt"])
-        
+        print("Shutting down. Goodbye!")
+        proc = subprocess.Popen(["sudo", "halt"])
+
+try:
+    GPIO.add_event_detect(shutdown_pin, GPIO.FALLING, callback=shutdown, bouncetime=5000)
+    while 1:
+        time.sleep(1)
+
 except KeyboardInterrupt:
   GPIO.cleanup()
   print "\n"
